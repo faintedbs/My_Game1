d3f20f3b05371fef68809a24ef8c93fe7c66b03c
faintedbs <aegarland717@gmail.com>
2023-03-16 12:40:07 -0700

    added score and collision

--- my_game/Sprites.py
+++ my_game/Sprites.py
@@ -6,71 +6,22 @@ from settings import *
 
 vec = pg.math.Vector2
 
+from random import randint
+
 # player class
 
 class Player(Sprite):
     def __init__(self):
         Sprite.__init__(self)
         self.image = pg.Surface((50,50))
-        self.image.fill(BLACK)
-        self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH/2, HEIGHT/2)
-        self.pos = vec(WIDTH/2, HEIGHT/2)
-        self.vel = vec(0,0)
-        self.acc = vec(0,0)
-        self.cofric = -.12
-        self.canjump = False
-    def input(self):
-        keystate = pg.key.get_pressed()
-
-        if keystate[pg.K_a]:
-            self.acc.x = -PLAYER_ACC
-        if keystate[pg.K_w]:
-            self.acc.y = -PLAYER_ACC
-        if keystate[pg.K_d]:
-            self.acc.x = PLAYER_ACC
-        if keystate[pg.K_s]:
-            self.acc.y = PLAYER_ACC
-        
-    def inbounds(self):
-        if self.rect.x > WIDTH - 50:
-            self.pos.x = WIDTH - 25
-            self.vel.x = 0
-            print("I am off the right side of the screem")
-        if self.rect.x < 0 + 50:
-            self.pos.x = 0 + 25
-            self.vel.x = 0
-            print("I am off the left side of the screem")
-        if self.rect.y > HEIGHT:
-            print("I am off the bellow of the screem")
-        if self.rect.y  < 0:
-            print("I am off the above of the screem")
-
-            
-    def update(self):
-        self.inbounds()
-        self.acc = self.vel * PLAYER_FRICTION
-        self.input()
-        self.vel += self.acc
-        self.pos += self.vel + 0.5 * self.acc
-        self.rect.center = self.pos
-
-class Mob(Sprite):
-    def __init__(self,width,height):
-        Sprite.__init__(self)
-        self.width = width
-        self.height = height
-        self.image = pg.Surface((self.width,self.height))
         self.image.fill(RED)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH/2, HEIGHT/2)
-        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.pos = vec(0, 0)
         self.vel = vec(0,0)
         self.acc = vec(0,0)
-        self.cofric = -0.3
+        self.cofric = 0.1
         self.canjump = False
-
-
     def input(self):
         keystate = pg.key.get_pressed()
 
@@ -94,32 +45,46 @@ class Mob(Sprite):
             print("i am off the bottom of the screen")
         if self.rect.y < 0:
             print("i am off the top of the screen...")
-    
-    def behavior(self):
-        self.acc.y = -MOB_ACC
-        if self.rect.x > WIDTH - 50:
-            self.pos.x = WIDTH - 25
-            self.vel.x = 0
-            print("i am off the right side of the screen...")
-        if self.rect.x < 0:
-            print("i am off the left side of the screen...")
-        if self.rect.y > HEIGHT:
-            print("i am off the bottom of the screen")
-        if self.rect.y < 0:
-            self.acc *= -1
-            self.vel *= -1
-            self.pos <= 0
-        
-            
-            print("i am off the top of the screen...")
-
-
-
 
     def update(self):
+        self.inbounds()
         self.acc = self.vel * PLAYER_FRICTION
+        self.input()
         self.vel += self.acc
-        self.behavior()
         self.pos += self.vel + 0.5 * self.acc
         self.rect.center = self.pos
 
+class Mob(Sprite):
+    def __init__(self,width,height,color):
+        Sprite.__init__(self)
+        self.width = width
+        self.height = height
+        self.image = pg.Surface((self.width,self.height))
+        self.color = color
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.pos = vec(WIDTH, HEIGHT)
+        self.vel = vec(randint(1,5),randint(1,5))
+        self.acc = vec(1,1)
+        self.cofric = 0.01
+    # ...
+    def inbounds(self):
+        if self.rect.x > WIDTH:
+            self.vel.x *= -1
+            # self.acc = self.vel * -self.cofric
+        if self.rect.x < 0:
+            self.vel.x *= -1
+            # self.acc = self.vel * -self.cofric
+        if self.rect.y < 0:
+            self.vel.y *= -1
+            # self.acc = self.vel * -self.cofric
+        if self.rect.y > HEIGHT:
+            self.vel.y *= -1
+            # self.acc = self.vel * -self.cofric
+    def update(self):
+        self.inbounds()
+        # self.pos.x += self.vel.x
+        # self.pos.y += self.vel.y
+        self.pos += self.vel
+        self.rect.center = self.pos
--- my_game/main.py
+++ my_game/main.py
@@ -3,7 +3,7 @@
 # This file was modified from: Chris Cozort
 # Sources: http://kidscancode.org/blog/2016/08/pygame_1-1_getting-started/
 # Sources: 
-
+# Testing github
 # import libs
 import pygame as pg
 import random
@@ -17,10 +17,19 @@ from Sprites import *
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, "img")
 
+def draw_text(text, size, color, x, y):
+    font_name = pg.font.match_font('arial')
+    font = pg.font.Font(font_name, size)
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x,y)
+    screen.blit(text_surface, text_rect)
+    
+
+
 def get_mouse_now():
     x,y = pg.mouse.get_pos()
     return (x,y)
-
 
 # init pygame and create window
 pg.init()
@@ -33,12 +42,23 @@ clock = pg.time.Clock()
 all_sprites = pg.sprite.Group()
 enemies = pg.sprite.Group()
 player = Player()
-enemy1 = Mob(60,80)
-all_sprites.add(enemy1)
+
+# enemy1 = Mob(60,80)
+# enemy2 = Mob(70,80)
+# enemy3 = Mob(80,100)
+
+
+# all_sprites.add(enemy1)
+# all_sprites.add(enemy2)
+# all_sprites.add(enemy3)
 all_sprites.add(player)
 # all_sprites.add(testSprite)
 
-
+for i in range(1,20):
+    m = Mob(randint(30,90),randint(30,90),randint(0,255))
+    enemies.add(m)
+    all_sprites.add(m)
+    
 # game loop
 
 while RUNNING:
@@ -56,11 +76,16 @@ while RUNNING:
 
     blocks_hit_list = pg.sprite.spritecollide(player, enemies, True)
     for block in blocks_hit_list:
-        # print(enemies)
+        print(block)
+        SCORE += 1
+        print(SCORE)
+        
+        
         pass
     ### draw and render section of game loop
-    screen.fill(BLUE)
+    screen.fill(GREEN)
     all_sprites.draw(screen)
+    draw_text("Score :" + str(SCORE), 22, BLACK, WIDTH/2, WIDTH/8)
     # double buffering draws frames for entire screen
     pg.display.flip()
     # pygame.display.update() -> only updates a portion of the screen
